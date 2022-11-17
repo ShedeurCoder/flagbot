27d2e6705ba1c73c6feaf0f324569258b052c22c
Shedeur B <81215635+ShedeurCoder@users.noreply.github.com>
2022-11-18 00:52:47 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
                 { name: '/countrylist', value: `Shows the countries included in this bot.`, inline: true },
                 { name: '/flag', value: `Classic flag quiz`, inline: true },
                 { name: '/minute', value: `Minute flag quiz`, inline: true },
-                { name: '/rank', value: `Check your rank for flag high scores`, inline: true },
+                { name: '/rank', value: `Check your rank (or someone else's) for flag high scores`, inline: true },
                 { name: '/reportbug', value: `Shows a link to report a bug/problem`, inline: true },
                 { name: '/invite', value: `Gives a link to invite the bot to your server`, inline: true },
                 { name: 'Invite me to your server!', value: `[Invite now!](https://discord.com/api/oauth2/authorize?client_id=1042497749079314514&permissions=294205376512&scope=bot%20applications.commands)`, inline: false }
